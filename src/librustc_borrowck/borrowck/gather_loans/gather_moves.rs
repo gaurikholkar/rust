248a5f80c74a3f93f977019e77b953e52a9f1462
gaurikholkar <f2013002@goa.bits-pilani.ac.in>
2017-04-21 13:07:22 -0700

    minor fixes

--- src/librustc_borrowck/borrowck/gather_loans/gather_moves.rs
+++ src/librustc_borrowck/borrowck/gather_loans/gather_moves.rs
@@ -129,6 +129,7 @@ pub fn gather_move_from_pat<'a, 'tcx>(bccx: &BorrowckCtxt<'a, 'tcx>,
                                       move_error_collector: &mut MoveErrorCollector<'tcx>,
                                       move_pat: &hir::Pat,
                                       cmt: mc::cmt<'tcx>) {
+    let source = get_pattern_source(bccx.tcx,move_pat);
     let pat_span_path_opt = match move_pat.node {
         PatKind::Binding(_, _, ref path1, _) => {
             Some(MoveSpanAndPath {
